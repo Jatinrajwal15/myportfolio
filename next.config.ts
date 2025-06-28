d5535de8a6a7383aa49c5b1f03b7717aebb32b77
jatinrajwal15 <rajwaljatin15@gmail.com>
2025-06-28 23:57:58 +0530

    Updated code

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  // output: 'export',
-  // images: {
-  //   unoptimized: true,
-  // },
-  // trailingSlash: true, 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: '/myportfolio',
+  assetPrefix: '/myportfolio',
 }
 
-export default nextConfig
+export default nextConfig;
